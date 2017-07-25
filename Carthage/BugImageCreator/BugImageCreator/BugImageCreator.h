a6e2bd4edb63febf5e9c361c434db969bb27433e
Koji Murata <koji.murata@dena.com>
2017-07-25 14:27:40 +0900

    carthage project

---
+++ Carthage/BugImageCreator/BugImageCreator/BugImageCreator.h
@@ -0,0 +1,19 @@
+//
+//  BugImageCreator.h
+//  BugImageCreator
+//
+//  Created by Koji Murata on 2017/07/25.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BugImageCreator.
+FOUNDATION_EXPORT double BugImageCreatorVersionNumber;
+
+//! Project version string for BugImageCreator.
+FOUNDATION_EXPORT const unsigned char BugImageCreatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugImageCreator/PublicHeader.h>
+
+
