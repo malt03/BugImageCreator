c50b23a7462558cb40eec9f8421464b6143f8bca
Koji Murata <koji.murata@dena.com>
2017-07-25 14:25:05 +0900

    ignore

--- Example/Pods/Target Support Files/BugImageCreator/BugImageCreator-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double BugImageCreatorVersionNumber;
-FOUNDATION_EXPORT const unsigned char BugImageCreatorVersionString[];
-
--- Example/Pods/Target Support Files/Pods-BugImageCreator_Example/Pods-BugImageCreator_Example-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_BugImageCreator_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BugImageCreator_ExampleVersionString[];
-
